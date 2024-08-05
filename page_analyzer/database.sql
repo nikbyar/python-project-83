eef67fec032e5d60902d074f27f1ca0ecaa12132
ivan <nikbyar@gmail.com>
2024-08-06 03:17:30 +0400

    edit table url_checks

--- page_analyzer/database.sql
+++ page_analyzer/database.sql
@@ -11,5 +11,5 @@ CREATE TABLE IF NOT EXISTS url_checks (
     h1 VARCHAR(255),
     title VARCHAR(255),
     description VARCHAR(255),
-    created_at DATE REFERENCES urls(created_at) NOT NULL
+    created_at DATE
     );
