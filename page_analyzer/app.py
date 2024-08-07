e5b36834a82c8a11165677cf7e7dd8fd003d4984
ivan <nikbyar@gmail.com>
2024-08-07 05:01:28 +0400

    fixing linter errors

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,8 @@
 import psycopg2
 import os
 import requests
-from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
+from flask import Flask, render_template, request, redirect, \
+    url_for, flash, get_flashed_messages
 from psycopg2.extras import DictCursor
 from dotenv import load_dotenv
 from urllib.parse import urlparse
@@ -20,7 +21,6 @@ def create_table():
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor() as cursor:
             with open('page_analyzer/database.sql', 'r') as f:
-            # with open('database.sql', 'r') as f:
                 cursor.execute(f.read())
                 conn.commit()
 
@@ -33,12 +33,13 @@ def add_to_urls(url):
             sites = [site[0] for site in cursor.fetchall()]
             if url not in sites:
                 cursor.execute("""
-                                    INSERT INTO urls (name) VALUES (%s) RETURNING id
+                                INSERT INTO urls (name) VALUES (%s) RETURNING id
                                 """, (url,))
                 conn.commit()
                 return cursor.fetchone()[0], 'added_successfully'
             else:
-                cursor.execute("SELECT (id) FROM urls WHERE name = (%s)", (url,))
+                cursor.execute("SELECT (id) FROM urls WHERE name = (%s)",
+                               (url,))
                 return cursor.fetchone()[0], 'already_exists'
 
 
@@ -52,7 +53,6 @@ def read_from_database(table_name, id):
             if not values:
                 values = [[] for i in range(len(column_names))]
             return dict(zip(column_names, values))
-
 
 
 def read_full_from_database(table_name, id='all'):
@@ -74,8 +74,8 @@ def add_to_url_checks(url_id, status_code, h1, title, description):
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor() as cursor:
             cursor.execute("""
-                                INSERT INTO url_checks 
-                                (url_id, status_code, h1, title, description) 
+                                INSERT INTO url_checks
+                                (url_id, status_code, h1, title, description)
                                 VALUES (%s, %s, %s, %s, %s)
                             """, (url_id, status_code, h1, title, description))
             conn.commit()
@@ -86,10 +86,11 @@ def merge_tables():
         with conn.cursor() as cursor:
             cursor.execute("""
                                 SELECT DISTINCT ON (urls.id)
-                                    urls.id, urls.name, 
+                                    urls.id, urls.name,
                                     checks.status_code, checks.created_at
                                 FROM urls
-                                LEFT JOIN url_checks AS checks ON urls.id = checks.url_id
+                                LEFT JOIN url_checks AS checks
+                                ON urls.id = checks.url_id
             """)
             conn.commit()
             values = cursor.fetchall()
@@ -107,7 +108,8 @@ def check_tags(response):
 
     description = None
     if soup.find('meta', attrs={'name': 'description'}):
-        description = soup.find('meta', attrs={'name': 'description'}).get('content')
+        description = soup.find('meta', attrs={'name': 'description'}).\
+            get('content')
 
     return h1, title, description
 
@@ -118,13 +120,11 @@ def index():
     return render_template('index.html', messages=messages)
 
 
-
 @app.route('/urls', methods=['GET', 'POST'])
 def get_urls():
     url = request.form.get('url')
     if not url:
         data = merge_tables()
-
         return render_template('urls.html', data=data)
     if not validate_url(url) or len(url) > 255:
         flash('Некорректный URL', 'alert-danger')
@@ -139,18 +139,13 @@ def get_urls():
     return redirect(url_for('get_url', id=id))
 
 
-        # flash('Некорректный URL', 'alert-success')
-        # flash('Некорректный URL', 'alert-warning')
-        # flash('Некорректный URL', 'alert-info')
-
-
-
 @app.route('/urls/<int:id>')
 def get_url(id):
     url = read_from_database('urls', id)
     messages = get_flashed_messages(with_categories=True)
     checks = read_full_from_database('url_checks', id)
-    return render_template('url.html', url=url, messages=messages, checks=checks)
+    return render_template('url.html', url=url, checks=checks,
+                           messages=messages)
 
 
 @app.post('/urls/<int:url_id>/checks')
@@ -160,14 +155,14 @@ def check_url(url_id):
         response = requests.get(url)
         if response.status_code == 200:
             h1, title, description = check_tags(response)
-            add_to_url_checks(url_id, response.status_code, h1, title, description)
+            add_to_url_checks(url_id, response.status_code, h1,
+                              title, description)
             flash('Страница успешно проверена', 'alert-success')
         else:
             flash('Произошла ошибка при проверке', 'alert-danger')
+
     except requests.RequestException:
         flash('Произошла ошибка при проверке', 'alert-danger')
-
-
 
     return redirect(url_for('get_checked_url', url_id=url_id))
 
@@ -177,21 +172,8 @@ def get_checked_url(url_id):
     checks = read_full_from_database('url_checks', url_id)
     url = read_from_database('urls', url_id)
     messages = get_flashed_messages(with_categories=True)
-    return render_template('url.html', url=url, checks=checks, messages=messages)
+    return render_template('url.html', url=url, checks=checks,
+                           messages=messages)
+
 
 create_table()
-
-
-
-# print(add_to_urls('ya11112.ru'))
-# print(add_to_urls('ya23.ru'))
-# print(add_to_urls('ya11.ru'))
-# print(read_full_from_database())
-# url_id = 3
-# id = add_to_url_checks(url_id)
-# print(read_from_database(id, 'url_checks'))
-
-
-
-
-
