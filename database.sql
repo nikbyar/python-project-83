908a9f1ee737d795c47920cd93ff6ace07782381
ivan <nikbyar@gmail.com>
2024-08-08 06:09:44 +0400

    sql edit

--- database.sql
+++ database.sql
@@ -1,17 +1,17 @@
 DROP TABLE IF EXISTS urls, url_checks;
 
 CREATE TABLE IF NOT EXISTS urls (
-    id SERIAL PRIMARY KEY,
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) NOT NULL UNIQUE,
     created_at DATE DEFAULT CURRENT_DATE
 );
 
---CREATE TABLE IF NOT EXISTS url_checks (
---    id SERIAL PRIMARY KEY,
---    url_id INT REFERENCES urls(id) NOT NULL,
---    status_code INT,
---    h1 VARCHAR(255),
---    title VARCHAR(255),
---    description TEXT,
---    created_at DATE DEFAULT CURRENT_DATE
---    );
+CREATE TABLE IF NOT EXISTS url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INT REFERENCES urls(id) NOT NULL ON DELETE CASCADE,
+    status_code INT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at DATE DEFAULT CURRENT_DATE
+    );
