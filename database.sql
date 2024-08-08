a8bd42abbc53a0525fc504466390bbbcb98a6f72
ivan <nikbyar@gmail.com>
2024-08-08 06:14:21 +0400

    sql edit

--- database.sql
+++ database.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS urls (
 
 CREATE TABLE IF NOT EXISTS url_checks (
     id SERIAL PRIMARY KEY,
-    url_id INT REFERENCES urls(id) NOT NULL ON DELETE CASCADE,
+    url_id INT REFERENCES urls(id) NOT NULL,
     status_code INT,
     h1 VARCHAR(255),
     title VARCHAR(255),
